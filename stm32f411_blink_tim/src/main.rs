1617df28971df70e569ce22201d08ba53c2661e7
Василий Николаев <tabaqui.vn@gmail.com>
2025-06-14 10:03:53 +0300

    pwm?

--- stm32f411_blink_tim/src/main.rs
+++ stm32f411_blink_tim/src/main.rs
@@ -1,28 +1,39 @@
 #![no_std]
 #![no_main]
 
-use cortex_m as _;
+use cortex_m::{self as _, asm::nop};
+// use defmt::info;
 use defmt_rtt as _;
 
 
 use cortex_m_rt::entry;
 // use cortex_m::asm;
 use panic_halt as _;
-use stm32f4xx_hal::{gpio::GpioExt, pac as Pa};
+use stm32f4xx_hal::{hal::pwm::SetDutyCycle, pac::{self}, prelude::*};
+
 
 
 #[entry]
 fn main() -> ! {
-    let d = Pa::Peripherals::take().unwrap();
-    let a = d.GPIOC.split();
-    let mut l = a.pc13.into_push_pull_output();
-    // let val = 3;
+    let hp = pac::Peripherals::take().unwrap();
+
+    let rcc = hp.RCC.constrain();
+
+    let clocks = rcc.cfgr.use_hse(1.kHz()).freeze();
+
+    let (_, (pwm_ch1, ..)) = hp.TIM1.pwm_us(100.micros(), &clocks);
+    // let (pwm_manager, (pwm_ch1, ..)) = hp.TIM1.pwm_hz(100.kHz(), &clocks);
+
+    
+    let gpioa = hp.GPIOA.split();
+
+    // let s = gpioa.pa8.in o_alternate();
+    let mut c1 = pwm_ch1.with(gpioa.pa8);
+    c1.set_duty_cycle_percent(10).unwrap();
+
+    c1.enable();
+
     loop {
-        // if 6 < val {
-            // let v = d.GPIOA.split();
-            // if 6 > val {
-                l.toggle();
-            // };
-        // }
+         nop();
     }
 }
