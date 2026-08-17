ccade8a68c6a33aa1f422efe4e8a34b0511406d9
Василий Николаев <tabaqui.vn@gmail.com>
2026-08-17 11:53:02 +0300

    Value calculated, filtered, send and received

--- stm32g030_pwm_adc/src/main.rs
+++ stm32g030_pwm_adc/src/main.rs
@@ -21,6 +21,11 @@ use embassy_stm32::{bind_interrupts, dma, Peri};
 use embassy_time::{Duration, Ticker, Timer};
 use panic_probe as _;
 use stm32g030::PidCalc;
+use embassy_sync::channel::Channel;
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+
+
+static SHARED_CHANNEL: Channel<ThreadModeRawMutex, f32, 8> = Channel::new();
 
 // static ADCC:
 
@@ -37,22 +42,14 @@ async fn main(_spawner: Spawner) {
 
     // let mut read_buffer = unsafe { &mut DMA_BUF[..] };
 
-    let mut adc = Adc::new_with_clock(p.ADC1, Clock::Async { div: Presc::DIV1 });
-    let mut adc_pin1 = p.PA2.degrade_adc();
-    let mut adc_pin2 = p.PA3.degrade_adc();
+    let adc = Adc::new_with_clock(p.ADC1, Clock::Async { div: Presc::DIV1 });
+    let adc_pin1 = p.PA2.degrade_adc();
+    let adc_pin2 = p.PA3.degrade_adc();
 
-    // let mut vrefint = adc.enable_vrefint();
-    // let vrefint_sample = adc.blocking_read(&mut vrefint, SampleTime::CYCLES160_5);
-    // let convert_to_millivolts = |sample: u16| {
-    //     // From https://www.st.com/resource/en/datasheet/stm32g031g8.pdf
-    //     // 6.3.3 Embedded internal reference voltage
-    //     const VREFINT_MV: u32 = 1212; // mV
 
-    //     (u32::from(sample) * VREFINT_MV / u32::from(vrefint_sample)) as u16
-    // };
 
     let pwm_pin = PwmPin::new(p.PA8, OutputType::PushPull);
-    let mut pwm = SimplePwm::new(
+    let pwm = SimplePwm::new(
         p.TIM1,
         Some(pwm_pin),
         None,
@@ -74,41 +71,17 @@ async fn main(_spawner: Spawner) {
     // let mut calc_pid = false;
     // Timer::after_secs(5).await;
 
-    //     let mut read_buffer = unsafe { &mut DMA_BUF[..] };
 
-    //     let mut vrefint = adc.enable_vrefint();
+    let dma: Peri<'_, peripherals::DMA1_CH1> = p.DMA1_CH1;
 
-    let mut dma: Peri<'_, peripherals::DMA1_CH1> = p.DMA1_CH1;
-
-    // let h: core::array::IntoIter<(&mut AnyAdcChannel<'_, ADC1>, SampleTime), 2> = [(&mut adc_pin1, SampleTime::CYCLES160_5), (&mut adc_pin2, SampleTime::CYCLES160_5)].into_iter();
-
-    //  adc.read(
-    //     dma.reborrow(),
-    //     Irqs,
-    //     h,
-    //     &mut read_buffer,
-    // )
-    // .await;
     _spawner.spawn(calc_adc_diff(dma, pwm, adc, adc_pin1, adc_pin2).unwrap());
 
     loop {
-        // pwm.ch1().disable();
-        Timer::after_micros(100).await;
-        nop();
-        // Timer::after_micros(600).await;
-        // let v = adc.blocking_read(&mut adc_pin1, SampleTime::CYCLES7_5);
-        // let v2 = adc.blocking_read(&mut adc_pin2, SampleTime::CYCLES7_5);
-        // let d: f32 = convert_to_millivolts(v2) as f32 - convert_to_millivolts(v) as f32;
-        // adc_values[i] = d;
-        // if i > 18 {
-        //     let avg = adc_values.iter().sum::<f32>() / 20.0;
-        //     i = 0;
-        //     info!("avg {}", avg);
-        // }
-        // i += 1;
-        // // info!("d {}", d);
+        let filtred_diff_mv = SHARED_CHANNEL.receiver().receive().await;
 
-        // pwm.ch1().enable();
+        info!("Received {}", filtred_diff_mv);
+        
+
 
         // Timer::after_millis(100).await;
 
@@ -124,10 +97,20 @@ async fn calc_adc_diff(
     mut adc_pin1: AnyAdcChannel<'static, peripherals::ADC1>,
     mut adc_pin2: AnyAdcChannel<'static, peripherals::ADC1>,
 ) {
-    info!("spawning&&&");
     let mut adc_ticker = Ticker::every(Duration::from_millis(100));
 
-    let mut read_buffer = unsafe { &mut DMA_BUF[..] };
+    let mut r_buffer = unsafe { &mut DMA_BUF[..] };
+
+    let mut filtred_diff_mv = 0.0;
+
+    let mut vrefint = adc.enable_vrefint();
+    let vrefint_sample = adc.blocking_read(&mut vrefint, SampleTime::CYCLES160_5);
+    let to_mv = |sample: u16| {
+        // From https://www.st.com/resource/en/datasheet/stm32g031g8.pdf
+        // 6.3.3 Embedded internal reference voltage
+        const VREFINT_MV: u32 = 1212;
+        (u32::from(sample) * VREFINT_MV / u32::from(vrefint_sample)) as u16
+    };
 
     loop {
         adc_ticker.next().await;
@@ -140,14 +123,19 @@ async fn calc_adc_diff(
         ]
         .into_iter();
 
-        adc.read(dma.reborrow(), Irqs, adc_channels, &mut read_buffer)
+        adc.read(dma.reborrow(), Irqs, adc_channels, &mut r_buffer)
             .await;
 
-        let vrefint = read_buffer[0];
-        let measured = read_buffer[1];
-        info!("vrefint: {}", vrefint);
-        info!("measured: {}", measured);
-        pwm.ch1().enable();
+        let adc_v_ref_1 = r_buffer[0];
+        let adc_v_ref_2 = r_buffer[1];
 
+        let diff_mv = to_mv(adc_v_ref_1) as f32 - to_mv(adc_v_ref_2) as f32;
+
+        filtred_diff_mv = (1.0 - 0.2) * filtred_diff_mv + 0.2 * diff_mv;
+        if let Err(e) = SHARED_CHANNEL.sender().try_send(filtred_diff_mv) {
+            error!("Channel is {}", e);
+        };
+        // info!("filtred_diff_mv: {}", filtred_diff_mv);
+        pwm.ch1().enable();
     }
 }
